6df38ea8f3a3e7d0ab8d8afd6876c36cb07fd654
hyun <apfhd0257@naver.com>
2025-08-24 17:02:04 +0900

    회원 애플리케이션 서비스 테스트

---
+++ src/test/java/tobyspring/splearn/application/provided/MemberRegisterTest.java
@@ -0,0 +1,37 @@
+package tobyspring.splearn.application.provided;
+
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestConstructor;
+import tobyspring.splearn.SplearnTestConfiguration;
+import tobyspring.splearn.domain.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@SpringBootTest
+@Transactional
+@Import(SplearnTestConfiguration.class)
+//@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+public record MemberRegisterTest(MemberRegister memberRegister) {
+
+    @Test
+    void register(){
+        Member member = memberRegister.register(MemberFixture.createMemberRegisterRequest());
+
+        assertThat(member.getId()).isNotNull();
+        assertThat(member.getStatus()).isEqualTo(MemberStatus.PENDING);
+    }
+
+    @Test
+    void duplicateEmailFail(){
+        Member member = memberRegister.register(MemberFixture.createMemberRegisterRequest());
+
+        assertThatThrownBy(() -> memberRegister.register(MemberFixture.createMemberRegisterRequest()))
+                .isInstanceOf(DuplicateEmailException.class);
+    }
+
+}
